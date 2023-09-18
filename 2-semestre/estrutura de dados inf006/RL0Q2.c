8b41cfe8740bf0bb78002e55997e3b975fd51724
juliobr1 <julio01.brito@gmail.com>
2023-09-18 01:20:41 -0300

    Alteracao

--- 2-semestre/estrutura de dados inf006/RL0Q2.c
+++ 2-semestre/estrutura de dados inf006/RL0Q2.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #define MAX_LINE 300
 
-int isFloat(char *str) {
+int isFloat(char *str){
     char *endptr;
     strtod(str, &endptr);
     if (*endptr == '\0' || (*endptr == '.' && *(endptr + 1) != '\0')) {
@@ -13,7 +14,7 @@ int isFloat(char *str) {
     return 0;
 }
 
-int isInt(char *str) {
+int isInt(char *str){
     char *endptr;
     strtol(str, &endptr, 10);
     if (*endptr == '\0') {
@@ -22,42 +23,120 @@ int isInt(char *str) {
     return 0;
 }
 
-int isCoordinate(char *str) {
+int isCoordinate(char *str){
     int len = strlen(str);
-    if (len >= 3 && str[0] == '(' && str[len - 1] == ')' && strchr(str, ',') != NULL) {
+    if (len >= 3 && str[0] == '(' && strchr(str, ',') != NULL) {
         return 1;
     }
     return 0;
 }
 
-int main(){
-  //Ler o arquivo
-  FILE* file_in = fopen("../L0Q2.in","r");
-  FILE* file_out = fopen("L0Q2.out","w");
+float distanciazero(float pX, float pY){
+    float distance;
+    float y0 = 0.0;
+    float x0 = 0.0;
+    
+    distance = sqrt(pow((x0 - pX), 2) + pow((y0 - pY), 2));
+    
+    return distance;
+}
 
-  if(file_in == NULL || file_out == NULL){
-    printf("Falha na criacao dos arquivos\n");
-    return EXIT_FAILURE;
-  }
-  else{
-    printf("Arquivos criados e abertos com sucesso\n");
-  }
+struct auxiliar {
+    char texto[50];
+    char inteiro[50];
+    char flutuante[50];
+    char ponto[25];
+    float distancia;
+};
 
-char * line = malloc(MAX_LINE * sizeof(char));
+ /*
+int ordenarstring(const void *a, const void *b){
+    int counta = 0;
+    int countb = 0;
+    char key[100];
+    char alf[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','w','y','z'};
+    char alfb[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','W','Y','Z'};
 
-fgets(line, MAX_LINE, file_in);
-
-char * token = strtok(line, " ");
-    while (token != NULL) {
-        if (isCoordinate(token)) {
-            printf("Coordenadas: %s\n", token);
-        } else if (isFloat(token)) {
-            printf("Float: %s\n", token);
-        } else if (isInt(token)) {
-            printf("Inteiro: %s\n", token);
-        } else {
-            printf("String: %s\n", token);
-        }
-        token = strtok(NULL, " ");
+    for (int i = 0, i <= 26 && counta == countb, i++){
+        if ((const char *)a[i] == alf[i] || (const char *)a[i] == alfb[i])
+            counta++;
+        if ((const char *)b[i] == alf[i] || (const char *)b[i] == alfb[i])
+            countb++;
+        if (counta > countb)
+            strncpy(key, (const char *)a, sizeof(key));
+            strncpy((const char *)a, (const char *)b, sizeof((const char *)a));
+            strncpy((const char *)b, key, sizeof((const char *)b));
+        
+        if (counta < countb)
+            strncpy(key, (const char *)a, sizeof(key));
+            strncpy((const char *)a, (const char *)b, sizeof((const char *)a));
+            strncpy((const char *)b, key, sizeof((const char *)b));
     }
+}
+*/
+
+int main() {
+    FILE* file_in = fopen("L0Q2.in","r");
+    FILE* file_out = fopen("L0Q2.out","w");
+
+    if(file_in == NULL || file_out == NULL){
+        printf("Falha na criacao dos arquivos\n");
+        return EXIT_FAILURE;
+    }
+    else{
+        printf("Arquivos criados e abertos com sucesso\n");
+    }
+
+    char * line = malloc(MAX_LINE * sizeof(char));
+
+    //while vem aqui
+    fgets(line, MAX_LINE, file_in);
+
+    struct auxiliar aux[100];
+    int texto_count = 0;
+    int inteiro_count = 0;
+    int flutuante_count = 0;
+    int ponto_count = 0;
+
+    char * token = strtok(line, " ");
+        while (token != NULL) {
+            if (isCoordinate(token)) {
+                strncpy(aux[ponto_count].ponto, token, sizeof(aux[ponto_count].ponto));
+                ponto_count++;
+            } else if (isInt(token)) {
+                strncpy(aux[inteiro_count].inteiro, token, sizeof(aux[inteiro_count].inteiro));
+                inteiro_count++;
+            } else if (isFloat(token)) {
+                strncpy(aux[flutuante_count].flutuante, token, sizeof(aux[flutuante_count].flutuante));
+                flutuante_count++;
+            } else {
+                strncpy(aux[texto_count].texto, token, sizeof(aux[texto_count].texto));
+                texto_count++;
+            }
+            token = strtok(NULL, " ");
+        }
+    
+    printf("str: ");
+    for (int i = 0; i < texto_count; i++){
+        printf("%s ", aux[i].texto);
+    }
+
+    printf("int: ");
+    for (int i = 0; i < inteiro_count; i++){
+        printf("%s ", aux[i].inteiro);
+    }
+
+    printf("float: ");
+    for (int i = 0; i < flutuante_count; i++){
+        printf("%s ", aux[i].flutuante);
+    }
+
+    printf("p: ");
+    for (int i = 0; i < ponto_count; i++){
+        printf("%s ", aux[i].ponto);
+    }
+
+    fclose(file_in);
+    fclose(file_out);
+
 }
